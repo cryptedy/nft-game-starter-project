6e6bd8af53d9645744dfa3a96eeec60050e844a2
Cryptedy <hoge@hoge.com>
2022-05-13 09:47:10 +0900

    update

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const CONTRACT_ADDRESS = "0xeeCaA81dfcb113c7a5103cfd128309546139A9a7";
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS , transformCharacterData };
